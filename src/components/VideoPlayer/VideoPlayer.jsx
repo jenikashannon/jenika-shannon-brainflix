51b5cbfabf2cf37b4bdfe23a36aaee13e5664fa8
Jenika Shannon <jenikashannon@pm.me>
2024-02-22 09:22:43 -0500

    add static video player html, styling, and responsive design

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -3,9 +3,11 @@ import videoPreview from "../../assets/images/Upload-video-preview.jpg";
 
 function VideoPlayer() {
 	return (
-		<video className='video-player' controls poster={videoPreview}>
-			<source src='' />
-		</video>
+		<div className='video-player-container'>
+			<video className='video-player' controls poster={videoPreview}>
+				<source src='' />
+			</video>
+		</div>
 	);
 }
 
