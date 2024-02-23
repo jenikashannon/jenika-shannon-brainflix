8824a9ece5b1c67a54c644b4f1ef6de867a18664
Jenika Shannon <jenikashannon@pm.me>
2024-02-23 17:04:10 -0500

    add responsive styling

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,6 +1,7 @@
 import "./CommentForm.scss";
 import Avatar from "../Avatar/Avatar";
 import Button from "../Button/Button";
+import userAvatar from "../../assets/images/Mohan-muruge.jpg";
 
 function CommentForm({ count, commentList, setCommentList }) {
 	return (
@@ -9,18 +10,20 @@ function CommentForm({ count, commentList, setCommentList }) {
 				{count === 1 ? `1 Comment` : `${count} Comments`}
 			</p>
 			<div className='comments__container'>
-				<Avatar />
+				<Avatar imageUrl={userAvatar} />
 				<form className='comment-form'>
-					<label className='comment-form__label' htmlFor='comment'>
-						JOIN THE CONVERSATION
-					</label>
-					<textarea
-						className='comment-form__field'
-						type='text-area'
-						id='comment'
-						name='comment'
-						placeholder='Add a new comment'
-					></textarea>
+					<div className='comment-form__input-container'>
+						<label className='comment-form__label' htmlFor='comment'>
+							JOIN THE CONVERSATION
+						</label>
+						<textarea
+							className='comment-form__field'
+							type='text-area'
+							id='comment'
+							name='comment'
+							placeholder='Add a new comment'
+						></textarea>
+					</div>
 					<Button type='comment' />
 				</form>
 			</div>
