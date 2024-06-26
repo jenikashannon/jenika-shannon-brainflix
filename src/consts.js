bdbe50376c51fefbee416a2d871e7f259848b1ab
Jenika Shannon <jenikashannon@pm.me>
2024-06-26 13:44:56 -0400

    correct api url

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://jenika-brainflix-backend-029be10389ae.herokuapp.com/";
+const baseUrl = "https://jenika-brainflix-backend-029be10389ae.herokuapp.com";
 const publicPath = `${baseUrl}/images`;
 
 export { baseUrl, publicPath };
